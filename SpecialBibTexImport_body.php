ea83584b91203ccf092027a800f9345ac3c90a1a
Steren <steren@creativecommons.org>
2009-01-18 19:41:36 +0100

    loop to catch data from post

--- SpecialBibTexImport_body.php
+++ SpecialBibTexImport_body.php
@@ -46,16 +46,16 @@ class SpecialBibTexImport extends SpecialPage {
       $titleObj = Title::makeTitle( NS_SPECIAL, 'BibTexImport' );
       $action = $titleObj->escapeLocalURL();
 
-
       $output_select='';
+
       $output_select.='<form enctype="multipart/form-data" method="post"  action="'.$action.'">';
       $output_select.='<table style="width: 100%; border :0; ">';
 
       $myBIB=new Bibliography($fileinfo['tmp_name']);
       foreach($myBIB->biblio["title"] as $bibkey=>$title) {
-          $output_select.='<tr> <td><input name="' . $bibkey . '" type="checkbox" /></td> <td>'.wfMsg( 'bibteximport-title' ).'</td> <td><input type="text" name="'. $bibkey .'_-_title" value="' . $title . '" size="60"/></td></tr>';
-          if(isset($myBIB->biblio["author"][$bibkey])) { $output_select.='<tr><td></td><td>'.wfMsg( 'bibteximport-author' ).'</td><td><input type="text" name="'. $bibkey .'_-_author" value="' . $myBIB->biblio["author"][$bibkey] . '" size="60"/></td></tr>'; }    
-          if(isset($myBIB->biblio["year"][$bibkey])) { $output_select.='<tr><td></td><td>'.wfMsg( 'bibteximport-year' ).'</td><td><input type="text" name="'. $bibkey .'_-_year" value="' . $myBIB->biblio["year"][$bibkey] . '" size="60"/></td></tr>'; }  
+          $output_select.='<tr> <td><input name="bibkey_-_' . $bibkey . '" type="checkbox" /></td> <td>'.wfMsg( 'bibteximport-title' ).'</td> <td><input type="text" name="title_-_'. $bibkey .'" value="' . $title . '" size="60"/></td></tr>';
+          if(isset($myBIB->biblio["author"][$bibkey])) { $output_select.='<tr><td></td><td>'.wfMsg( 'bibteximport-author' ).'</td><td><input type="text" name="author_-_'. $bibkey .'" value="' . $myBIB->biblio["author"][$bibkey] . '" size="60"/></td></tr>'; }    
+          if(isset($myBIB->biblio["year"][$bibkey])) { $output_select.='<tr><td></td><td>'.wfMsg( 'bibteximport-year' ).'</td><td><input type="text" name="year_-_'. $bibkey .'" value="' . $myBIB->biblio["year"][$bibkey] . '" size="60"/></td></tr>'; }  
           $extracted++;
       }
 
@@ -72,13 +72,48 @@ class SpecialBibTexImport extends SpecialPage {
     }
 
     function ImportArticles() {
-      global $IP;
-      //TO CHANGE require_once "$IP/includes/User.php";
-      $output = '';
+        global $IP;
+        //TO CHANGE require_once "$IP/includes/User.php";
+        $output = '';
 
-  foreach ($_POST as $key => $value) {
-    $output .= $key . addslashes(trim($value)) ."<br/>";
-  }
+        $articles = array();
+        $bibkey ='';
+        $title = ''; $summary = '';
+
+        //We first parse the post data
+        foreach ($_POST as $key => $value) {
+            $keyword_key = explode('_-_',$key);
+            if($keyword_key[0] == 'bibkey' && $value == 'on') { 
+                //if bibkey not empty create the page before doing more processing
+                if($bibkey !='') {
+                    //TOCHANGE
+                    $output .= 'TOCHANGE NEPAGE ' . $title .'<br/>' . $summary ;
+                }                
+
+                //prepare the new temp variables
+                $bibkey = $keyword_key[1]; 
+                $output.='TOCHANGE article created for key ' . $bibkey .'<br/>';
+                $title = ''; $summary = '';
+            }
+            else if( $keyword_key[1] == $bibkey && $keyword_key[0] == 'title' ) { 
+                $output .= 'TOCHANGE Title : ' . $value .'<br/>' ;
+                $title = $value;
+            }
+            else if( $keyword_key[1] == $bibkey && $keyword_key[0] == 'author' ) {
+                $output .= 'TOCHANGE Author : ' . $value .'<br/>' ;
+                $summary.= "author=" . $value . "\n";
+            }
+            else if( $keyword_key[1] == $bibkey && $keyword_key[0] == 'year' ) {
+                $output .= 'TOCHANGE Year : ' . $value .'<br/>' ;
+                $summary.= "year=" . $value . "\n";
+            }
+        }
+        //create the page
+        //TOCHANGE
+        $output .= 'TOCHANGE NEPAGE ' . $title .'<br/>' . $summary ;
+
+
+
 
       return $output;
     }
